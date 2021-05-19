5176946d84356bc2203dd0d67b70e61ba779d216
KennethanCeyer <kenneth@pigno.se>
2021-05-20 05:23:42 +0900

    perf: use syntactic instead of pow

--- grayscale/math/var.py
+++ grayscale/math/var.py
@@ -1,12 +1,11 @@
 from typing import List
 
 from grayscale.math import mean
-from grayscale.math import pow
 
 
 def var(nums: List[float]) -> float:
     mean_ = mean(nums)
     vsum = 0
     for n in nums:
-        vsum += pow((n - mean_), 2)
+        vsum += (n - mean_) ** 2
     return vsum / len(nums)
